dedc65d625f5ae6e4bb1717728333eb2ec6ee3a2
William Sasaki <65739117+wsasaki01@users.noreply.github.com>
2023-06-14 21:17:44 +0100

    Add shake and hitstop

--- p1/main.lua
+++ p1/main.lua
@@ -70,116 +70,138 @@ function _init()
     enemy_mt = {}
 
     enemy_limit = 10
+
+    sh_str = 0
+    hitstop = false
+    hs_count = 0
+    hs_frames = 3
 end
 
 function _update()
-    diff = {x=0,y=0}
+    if hitstop then
+        hs_count += 1
+        if hs_count == hs_frames then
+            hitstop = false
+            hs_count = 0
+        end
+    else
+        diff = {x=0,y=0}
 
-    if btn(1) and p.x<120 then
-        p.x+=1
-        diff.x+=1
-    end
-
-    if btn(0) and p.x>0 then
-        p.x-=1
-        diff.x-=1
-    end
-
-    if btn(2) and p.y>0 then
-        p.y-=1
-        diff.y-=1
-    end
-
-    if btn(3) and p.y<120 then
-        p.y+=1
-        diff.y+=1
-    end
-
-    if h.equipped then
-        h.x = p.x
-        h.y = p.y
-        h.d = atan2(diff.x, diff.y)
-    end
-
-    if p.charge == p.cooldown then
-        p.charge = true
-    end
-
-    if p.charge != true then
-        p.charge += 1
-    end
-
-    if btn(5) and h.equipped and p.charge==true and #attacks<1 then
-        p.charge = 0
-        create_attack("player", p_attack_length, p_attack_size)
-    end
-
-    h:check()
-
-    if btn(4) and h.equipped and (diff.x!=0 or diff.y!=0) then
-        h.thrown = true
-        h.equipped = false
-        h.v = 10
-        h.path = diff
-        create_attack("hammer", h_attack_length, h_attack_size)
-    end
-
-    if h.v < 1 then
-        h.thrown = false
-        h.v = 0
-        h.path={x=0, y=0}
-    end
-
-    if h.thrown then
-        local x = h.x+cos(h.d)*h.v
-        local y = h.y+sin(h.d)*h.v
-
-        if x>=128 then
-            h.x = 127
-        elseif x <= 0 then
-            h.x = 1
-        else
-            h.x = x
+        if btn(1) and p.x<120 then
+            p.x+=1
+            diff.x+=1
         end
 
-        if y>=128 then
-            h.y = 127
-        elseif y <= 0 then
-            h.y = 1
-        else
-            h.y = y
+        if btn(0) and p.x>0 then
+            p.x-=1
+            diff.x-=1
         end
 
-        h.v*=0.8
-    end
-
-    if #enemies < enemy_limit then
-        create_enemy()
-    end
-
-    for e in all(enemies) do
-        e:move()
-        e:die()
-    end
-    
-    p:sprite()
-    p:die()
-    p:invincibilty()
-
-    for a in all(attacks) do
-        if a.type == "player" then
-            attack_follow(a, p.x, p.y)
-        elseif a.type == "hammer" then
-            attack_follow(a, h.x, h.y)
+        if btn(2) and p.y>0 then
+            p.y-=1
+            diff.y-=1
         end
 
-        a:decay()
-    end
+        if btn(3) and p.y<120 then
+            p.y+=1
+            diff.y+=1
+        end
 
+        if h.equipped then
+            h.x = p.x
+            h.y = p.y
+            h.d = atan2(diff.x, diff.y)
+        end
+
+        if p.charge == p.cooldown then
+            p.charge = true
+        end
+
+        if p.charge != true then
+            p.charge += 1
+        end
+
+        if btn(5) and h.equipped and p.charge==true and #attacks<1 then
+            p.charge = 0
+            create_attack("player", p_attack_length, p_attack_size)
+        end
+
+        h:check()
+
+        if btn(4) and h.equipped and (diff.x!=0 or diff.y!=0) then
+            h.thrown = true
+            h.equipped = false
+            h.v = 10
+            h.path = diff
+            create_attack("hammer", h_attack_length, h_attack_size)
+        end
+
+        if h.v < 1 then
+            h.thrown = false
+            h.v = 0
+            h.path={x=0, y=0}
+        end
+
+        if h.thrown then
+            local x = h.x+cos(h.d)*h.v
+            local y = h.y+sin(h.d)*h.v
+
+            if x>=128 then
+                h.x = 127
+            elseif x <= 0 then
+                h.x = 1
+            else
+                h.x = x
+            end
+
+            if y>=128 then
+                h.y = 127
+            elseif y <= 0 then
+                h.y = 1
+            else
+                h.y = y
+            end
+
+            h.v*=0.8
+        end
+
+        if #enemies < enemy_limit then
+            create_enemy()
+        end
+
+        for e in all(enemies) do
+            e:move()
+            e:die()
+        end
+        
+        p:sprite()
+        p:die()
+        p:invincibilty()
+
+        for a in all(attacks) do
+            if a.type == "player" then
+                attack_follow(a, p.x, p.y)
+            elseif a.type == "hammer" then
+                attack_follow(a, h.x, h.y)
+            end
+
+            a:decay()
+        end
+    end
 end
 
 function _draw()
     cls(7)
+
+    camera(0, 0)
+
+    log({
+        p.charge,
+        p.score,
+        p.health
+    })
+
+    shake(0, 0)
 
     for a in all(attacks) do
         a:draw()
@@ -192,12 +214,6 @@ function _draw()
     end
 
     h:draw()
-
-    log({
-        p.charge,
-        p.score,
-        p.health
-    })
 end
 
 function create_enemy()
@@ -220,6 +236,8 @@ function create_enemy()
             for a in all(attacks) do
                 if collide(self.x, self.y, self.yw, self.xw, a.x, a.y, a.xw, a.yw) then
                     p.score+=1
+                    sh_str+=0.1
+                    hitstop = true
                     del(enemies, self)
                 end
             end
