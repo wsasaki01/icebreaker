146d826a18243224a8ea3832cf04ff2109fd83fe
William Sasaki <65739117+wsasaki01@users.noreply.github.com>
2024-01-29 19:38:18 +0000

    Hammer trail

--- v2/draw.lua
+++ v2/draw.lua
@@ -10,11 +10,12 @@ function _draw()
         p_x,p_y,1,1,p_flip
     )
 
+    if h_v > 0.5 then
+        line(h_prev_x+3, h_prev_y+3, h_x+3, h_y+3, 6)
+    end
+
     if (not h_held) spr(128, h_x, h_y)
     
-    if h_v > 0.5 then
-        line(h_prev_x+2, h_prev_y+2, h_x+2, h_y+2, 6)
-    end
     --rect(p_x,p_y+2,p_x+7,p_y+8)
     --rect(h_x,h_y,h_x+h_xw,h_y+h_yw)
 end
